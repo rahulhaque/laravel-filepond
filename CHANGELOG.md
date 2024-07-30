be17a3926122d4dfb8cb445e9cf9c3d41615b2e7
Rahul <rahulhaque07@gmail.com>
2024-07-30 23:31:50 +0600

    Large file processing in third party storage fixed 🐛

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,12 @@
 
 All notable changes to `laravel-filepond` will be documented in this file.
 
+## 8.0.1 - 2024-07-30
+
+- Fixed large file processing in third party storage 🐛.
+- Docker development environment isolated 🐳.
+- Filepond disk test cases added ✅.
+
 ## 8.0.0 - 2023-11-23
 
 - Locked package version to Laravel 8. 🔒
--- tests/Feature/FilepondFacadeTest.php
+++ tests/Feature/FilepondFacadeTest.php
@@ -17,176 +17,10 @@ class FilepondFacadeTest extends TestCase
     use RefreshDatabase;
 
     /** @test */
-    public function can_validate_null_filepond_file_upload()
-    {
-        Storage::disk(config('filepond.temp_disk', 'local'))->deleteDirectory(config('filepond.temp_folder', 'filepond/temp'));
-
-        $request = new Request([
-            'avatar' => null,
-        ]);
-
-        try {
-            $request->validate([
-                'avatar' => Rule::filepond('required|image|mimes:jpg|size:30'),
-            ]);
-        } catch (ValidationException $e) {
-            $this->assertEquals($e->errors(), ['avatar' => ['The avatar field is required.']]);
-        }
-    }
-
-    /** @test */
-    public function can_validate_after_filepond_file_upload()
-    {
-        Storage::disk(config('filepond.temp_disk', 'local'))->deleteDirectory(config('filepond.temp_folder', 'filepond/temp'));
-
-        $user = User::factory()->create();
-
-        $response = $this
-            ->actingAs($user)
-            ->post(route('filepond-process'), [
-                'avatar' => UploadedFile::fake()->image('avatar.png', 1024, 1024),
-            ], [
-                'Content-Type' => 'multipart/form-data',
-                'accept' => 'application/json',
-            ]);
-
-        $request = new Request([
-            'avatar' => $response->content(),
-        ]);
-
-        try {
-            $request->validate([
-                'avatar' => Rule::filepond('required|image|mimes:jpg|size:30'),
-            ]);
-        } catch (ValidationException $e) {
-            $this->assertEquals($e->errors(), [
-                'avatar' => [
-                    'The avatar must be a file of type: jpg.',
-                    'The avatar must be 30 kilobytes.',
-                ],
-            ]);
-        }
-    }
-
-    /** @test */
-    public function can_validate_after_multiple_filepond_file_upload()
-    {
-        Storage::disk(config('filepond.temp_disk', 'local'))->deleteDirectory(config('filepond.temp_folder', 'filepond/temp'));
-
-        $user = User::factory()->create();
-
-        $responses = [];
-
-        // Create 5 temporary file uploads
-        for ($i = 1; $i <= 5; $i++) {
-            $response = $this->actingAs($user)
-                ->post(route('filepond-process'), [
-                    'gallery' => UploadedFile::fake()->image('gallery-'.$i.'.png', 1024, 1024),
-                ], [
-                    'Content-Type' => 'multipart/form-data',
-                    'accept' => 'application/json',
-                ]);
-
-            $responses[] = $response->content();
-        }
-
-        $request = new Request([
-            'gallery' => $responses,
-        ]);
-
-        try {
-            $request->validate([
-                'gallery.*' => Rule::filepond('required|image|mimes:jpg|size:30'),
-            ]);
-        } catch (ValidationException $e) {
-            $this->assertEquals($e->errors(), [
-                'gallery.0' => [
-                    'The gallery.0 must be a file of type: jpg.',
-                    'The gallery.0 must be 30 kilobytes.',
-                ],
-                'gallery.1' => [
-                    'The gallery.1 must be a file of type: jpg.',
-                    'The gallery.1 must be 30 kilobytes.',
-                ],
-                'gallery.2' => [
-                    'The gallery.2 must be a file of type: jpg.',
-                    'The gallery.2 must be 30 kilobytes.',
-                ],
-                'gallery.3' => [
-                    'The gallery.3 must be a file of type: jpg.',
-                    'The gallery.3 must be 30 kilobytes.',
-                ],
-                'gallery.4' => [
-                    'The gallery.4 must be a file of type: jpg.',
-                    'The gallery.4 must be 30 kilobytes.',
-                ],
-            ]);
-        }
-    }
-
-    /** @test */
-    public function can_validate_after_nested_multiple_filepond_file_upload()
-    {
-        Storage::disk(config('filepond.temp_disk', 'local'))->deleteDirectory(config('filepond.temp_folder', 'filepond/temp'));
-
-        $user = User::factory()->create();
-
-        $responses = [];
-
-        // Create 5 temporary file uploads
-        for ($i = 1; $i <= 5; $i++) {
-            $response = $this->actingAs($user)
-                ->post(route('filepond-process'), [
-                    'galleries' => UploadedFile::fake()->image('gallery-'.$i.'.png', 1024, 1024),
-                ], [
-                    'Content-Type' => 'multipart/form-data',
-                    'accept' => 'application/json',
-                ]);
-
-            $responses[] = [
-                'title' => 'test-'.$i,
-                'image' => $response->content(),
-            ];
-        }
-
-        $request = new Request([
-            'galleries' => $responses,
-        ]);
-
-        try {
-            $request->validate([
-                'galleries.*.image' => Rule::filepond('required|image|mimes:jpg|size:30'),
-            ]);
-        } catch (ValidationException $e) {
-            $this->assertEquals($e->errors(), [
-                'galleries.0.image' => [
-                    'The galleries.0.image must be a file of type: jpg.',
-                    'The galleries.0.image must be 30 kilobytes.',
-                ],
-                'galleries.1.image' => [
-                    'The galleries.1.image must be a file of type: jpg.',
-                    'The galleries.1.image must be 30 kilobytes.',
-                ],
-                'galleries.2.image' => [
-                    'The galleries.2.image must be a file of type: jpg.',
-                    'The galleries.2.image must be 30 kilobytes.',
-                ],
-                'galleries.3.image' => [
-                    'The galleries.3.image must be a file of type: jpg.',
-                    'The galleries.3.image must be 30 kilobytes.',
-                ],
-                'galleries.4.image' => [
-                    'The galleries.4.image must be a file of type: jpg.',
-                    'The galleries.4.image must be 30 kilobytes.',
-                ],
-            ]);
-        }
-    }
-
-    /** @test */
     public function can_get_temporary_file_after_filepond_file_upload()
     {
         Storage::disk(config('filepond.temp_disk', 'local'))->deleteDirectory(config('filepond.temp_folder', 'filepond/temp'));
+        Storage::disk(config('filepond.disk', 'public'))->deleteDirectory('avatars');
 
         $user = User::factory()->create();
 
@@ -210,6 +44,7 @@ class FilepondFacadeTest extends TestCase
     public function can_get_data_url_after_filepond_file_upload()
     {
         Storage::disk(config('filepond.temp_disk', 'local'))->deleteDirectory(config('filepond.temp_folder', 'filepond/temp'));
+        Storage::disk(config('filepond.disk', 'public'))->deleteDirectory('avatars');
 
         $user = User::factory()->create();
 
@@ -234,6 +69,7 @@ class FilepondFacadeTest extends TestCase
     public function can_copy_filepond_file_upload_to_desired_location()
     {
         Storage::disk(config('filepond.temp_disk', 'local'))->deleteDirectory(config('filepond.temp_folder', 'filepond/temp'));
+        Storage::disk(config('filepond.disk', 'public'))->deleteDirectory('avatars');
 
         $user = User::factory()->create();
 
@@ -255,6 +91,7 @@ class FilepondFacadeTest extends TestCase
     public function can_move_filepond_file_upload_to_desired_location()
     {
         Storage::disk(config('filepond.temp_disk', 'local'))->deleteDirectory(config('filepond.temp_folder', 'filepond/temp'));
+        Storage::disk(config('filepond.disk', 'public'))->deleteDirectory('avatars');
 
         $user = User::factory()->create();
 
@@ -276,6 +113,7 @@ class FilepondFacadeTest extends TestCase
     public function can_copy_multiple_filepond_file_upload_to_desired_location()
     {
         Storage::disk(config('filepond.temp_disk', 'local'))->deleteDirectory(config('filepond.temp_folder', 'filepond/temp'));
+        Storage::disk(config('filepond.disk', 'public'))->deleteDirectory('galleries');
 
         $user = User::factory()->create();
 
@@ -305,6 +143,7 @@ class FilepondFacadeTest extends TestCase
     public function can_move_multiple_filepond_file_upload_to_desired_location()
     {
         Storage::disk(config('filepond.temp_disk', 'local'))->deleteDirectory(config('filepond.temp_folder', 'filepond/temp'));
+        Storage::disk(config('filepond.disk', 'public'))->deleteDirectory('galleries');
 
         $user = User::factory()->create();
 
