b7572e2cb8b4ab03a5ab0845b9368fcbf34e72f2
Rahul <rahulhaque07@gmail.com>
2025-09-12 17:58:38 +0600

    Chunk upload test added ✅

--- tests/Unit/FilepondDiskTest.php
+++ tests/Unit/FilepondDiskTest.php
@@ -19,10 +19,10 @@ class FilepondDiskTest extends TestCase
     #[Group('disk-test')]
     public function can_move_file_local_to_local()
     {
-        $pathToMove = 'moved/avatar';
+        $pathToMove = 'move_file_local_to_local/avatar';
 
-        Storage::disk(config('filepond.temp_disk', 'local'))->deleteDirectory(config('filepond.temp_folder', 'filepond/temp'));
-        Storage::disk(config('filepond.disk', 'public'))->deleteDirectory('moved');
+        Storage::disk(config('filepond.temp_disk'))->deleteDirectory(config('filepond.temp_folder', 'filepond/temp'));
+        Storage::disk(config('filepond.disk'))->deleteDirectory('move_file_local_to_local');
 
         $user = User::factory()->create();
 
@@ -39,7 +39,7 @@ class FilepondDiskTest extends TestCase
 
         $fileInfo = Filepond::field($response->content())->moveTo($pathToMove);
 
-        Storage::disk(config('filepond.disk', 'public'))->assertExists($fileInfo['location']);
+        Storage::disk(config('filepond.disk'))->assertExists($fileInfo['location']);
     }
 
     #[Test]
@@ -49,10 +49,10 @@ class FilepondDiskTest extends TestCase
         Config::set('filepond.temp_disk', 's3');
         Config::set('filepond.disk', 's3');
 
-        $pathToMove = 'moved/avatar';
+        $pathToMove = 'move_file_external_to_external/avatar';
 
-        Storage::disk(config('filepond.temp_disk', 'local'))->deleteDirectory(config('filepond.temp_folder', 'filepond/temp'));
-        Storage::disk(config('filepond.disk', 'public'))->deleteDirectory('moved');
+        Storage::disk(config('filepond.temp_disk'))->deleteDirectory(config('filepond.temp_folder', 'filepond/temp'));
+        Storage::disk(config('filepond.disk'))->deleteDirectory('move_file_external_to_external');
 
         $user = User::factory()->create();
 
@@ -69,7 +69,7 @@ class FilepondDiskTest extends TestCase
 
         $fileInfo = Filepond::field($response->content())->moveTo($pathToMove);
 
-        Storage::disk(config('filepond.disk', 'public'))->assertExists($fileInfo['location']);
+        Storage::disk(config('filepond.disk'))->assertExists($fileInfo['location']);
     }
 
     #[Test]
@@ -78,10 +78,10 @@ class FilepondDiskTest extends TestCase
     {
         Config::set('filepond.disk', 's3');
 
-        $pathToMove = 'moved/avatar';
+        $pathToMove = 'move_file_local_to_external/avatar';
 
-        Storage::disk(config('filepond.temp_disk', 'local'))->deleteDirectory(config('filepond.temp_folder', 'filepond/temp'));
-        Storage::disk(config('filepond.disk', 'public'))->deleteDirectory('moved');
+        Storage::disk(config('filepond.temp_disk'))->deleteDirectory(config('filepond.temp_folder', 'filepond/temp'));
+        Storage::disk(config('filepond.disk'))->deleteDirectory('move_file_local_to_external');
 
         $user = User::factory()->create();
 
@@ -98,7 +98,7 @@ class FilepondDiskTest extends TestCase
 
         $fileInfo = Filepond::field($response->content())->moveTo($pathToMove);
 
-        Storage::disk(config('filepond.disk', 'public'))->assertExists($fileInfo['location']);
+        Storage::disk(config('filepond.disk'))->assertExists($fileInfo['location']);
     }
 
     #[Test]
@@ -107,10 +107,10 @@ class FilepondDiskTest extends TestCase
     {
         Config::set('filepond.temp_disk', 's3');
 
-        $pathToMove = 'moved/avatar';
+        $pathToMove = 'move_file_external_to_local/avatar';
 
-        Storage::disk(config('filepond.temp_disk', 'local'))->deleteDirectory(config('filepond.temp_folder', 'filepond/temp'));
-        Storage::disk(config('filepond.disk', 'public'))->deleteDirectory('moved');
+        Storage::disk(config('filepond.temp_disk'))->deleteDirectory(config('filepond.temp_folder', 'filepond/temp'));
+        Storage::disk(config('filepond.disk'))->deleteDirectory('move_file_external_to_local');
 
         $user = User::factory()->create();
 
@@ -127,6 +127,109 @@ class FilepondDiskTest extends TestCase
 
         $fileInfo = Filepond::field($response->content())->moveTo($pathToMove);
 
-        Storage::disk(config('filepond.disk', 'public'))->assertExists($fileInfo['location']);
+        Storage::disk(config('filepond.disk'))->assertExists($fileInfo['location']);
+    }
+
+    #[Test]
+    #[Group('disk-test')]
+    public function can_chunk_upload_file_to_local(): void
+    {
+        $pathToMove = 'chunk_upload_file_to_local/document';
+
+        Storage::disk(config('filepond.temp_disk'))->deleteDirectory(config('filepond.temp_folder', 'filepond/temp'));
+        Storage::disk(config('filepond.disk'))->deleteDirectory('chunk_upload_file_to_local');
+
+        $user = User::factory()->create();
+
+        $content = str_repeat('f', 2 * 1024 * 1024); // Fake content 2MB (2097152 Bytes)
+        $chunks = mb_str_split($content, 256 * 1024); // Split into 8 chunks 256KB (262144 Bytes)
+
+        $initChunkUploadResponse = $this
+            ->actingAs($user)
+            ->post(route('filepond-process'), [], [
+                'upload-length' => mb_strlen($content),
+            ]);
+
+        $serverId = $initChunkUploadResponse->content();
+
+        $uploadOffset = 0;
+
+        foreach ($chunks as $chunk) {
+            $response = $this
+                ->actingAs($user)
+                ->call(
+                    method: 'PATCH',
+                    uri: route('filepond-patch', ['patch' => $serverId]),
+                    content: $chunk,
+                    server: $this->transformHeadersToServerVars([
+                        'content-length' => mb_strlen($chunk),
+                        'upload-length' => mb_strlen($content),
+                        'upload-name' => 'test-file.txt',
+                        'upload-offset' => $uploadOffset,
+                        'content-type' => 'application/offset+octet-stream',
+                    ])
+                );
+
+            if ($response->getContent() === 'Ok') {
+                $uploadOffset = (int) $response->headers->get('upload-offset');
+            }
+        }
+
+        $fileInfo = Filepond::field($serverId)->moveTo($pathToMove);
+
+        Storage::disk(config('filepond.disk'))->assertExists($fileInfo['location']);
+    }
+
+    #[Test]
+    #[Group('disk-test')]
+    public function can_chunk_upload_file_to_external(): void
+    {
+        Config::set('filepond.temp_disk', 's3');
+        Config::set('filepond.disk', 's3');
+
+        $pathToMove = 'chunk_upload_file_to_external/document';
+
+        Storage::disk(config('filepond.temp_disk'))->deleteDirectory(config('filepond.temp_folder', 'filepond/temp'));
+        Storage::disk(config('filepond.disk'))->deleteDirectory('chunk_upload_file_to_external');
+
+        $user = User::factory()->create();
+
+        $content = str_repeat('f', 2 * 1024 * 1024); // Fake content 2MB (2097152 Bytes)
+        $chunks = mb_str_split($content, 256 * 1024); // Split into 8 chunks 256KB (262144 Bytes)
+
+        $initChunkUploadResponse = $this
+            ->actingAs($user)
+            ->post(route('filepond-process'), [], [
+                'upload-length' => mb_strlen($content),
+            ]);
+
+        $serverId = $initChunkUploadResponse->content();
+
+        $uploadOffset = 0;
+
+        foreach ($chunks as $chunk) {
+            $response = $this
+                ->actingAs($user)
+                ->call(
+                    method: 'PATCH',
+                    uri: route('filepond-patch', ['patch' => $serverId]),
+                    content: $chunk,
+                    server: $this->transformHeadersToServerVars([
+                        'content-length' => mb_strlen($chunk),
+                        'upload-length' => mb_strlen($content),
+                        'upload-name' => 'test-file.txt',
+                        'upload-offset' => $uploadOffset,
+                        'content-type' => 'application/offset+octet-stream',
+                    ])
+                );
+
+            if ($response->getContent() === 'Ok') {
+                $uploadOffset = (int) $response->headers->get('upload-offset');
+            }
+        }
+
+        $fileInfo = Filepond::field($serverId)->moveTo($pathToMove);
+
+        Storage::disk(config('filepond.disk'))->assertExists($fileInfo['location']);
     }
 }
