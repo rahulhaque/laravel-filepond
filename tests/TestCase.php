d362afb522c9dc824141628bb15180fecaebd8c2
Rahul <rahulhaque07@gmail.com>
2025-10-25 20:55:10 +0600

    Dependencies + test cases updated 📦

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,19 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace RahulHaque\Filepond\Tests;
 
-use RahulHaque\Filepond\FilepondServiceProvider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Orchestra\Testbench\Attributes\WithMigration;
 
+#[WithMigration]
 class TestCase extends \Orchestra\Testbench\TestCase
 {
-    public function setUp(): void
-    {
-        parent::setUp();
-    }
+    use RefreshDatabase;
 
     protected function getPackageProviders($app)
     {
-        return [FilepondServiceProvider::class];
+        return [\RahulHaque\Filepond\FilepondServiceProvider::class];
     }
 
     protected function getPackageAliases($app)
@@ -21,12 +22,14 @@ class TestCase extends \Orchestra\Testbench\TestCase
         return ['Filepond' => \RahulHaque\Filepond\Facades\Filepond::class];
     }
 
-    protected function getEnvironmentSetUp($app)
+    /**
+     * Define environment setup.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return void
+     */
+    protected function defineEnvironment($app)
     {
-        include_once __DIR__.'/../database/migrations/create_fileponds_table.php.stub';
-        include_once __DIR__.'/database/migrations/create_users_table.php.stub';
-
-        (new \CreateUsersTable)->up();
-        (new \CreateFilepondsTable)->up();
+        $app->call([require __DIR__.'/../database/migrations/create_fileponds_table.php.stub', 'up']);
     }
 }
