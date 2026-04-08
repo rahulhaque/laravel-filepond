b0633c59723b4c30ab1a6066ac4cd6e182f637a9
Rahul <rahulhaque07@gmail.com>
2026-04-08 17:32:15 +0600

    Experimental `getDataURL()` method removed 🧹

--- src/AbstractFilepond.php
+++ src/AbstractFilepond.php
@@ -215,17 +215,4 @@ abstract class AbstractFilepond
             true
         );
     }
-
-    /**
-     * Create Data URL from filepond model
-     * More at - https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URIs
-     *
-     * @return string
-     *
-     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
-     */
-    protected function createDataUrl(Filepond $filepond)
-    {
-        return 'data:'.$filepond->mimetypes.';base64,'.base64_encode(Storage::disk($this->tempDisk)->get($filepond->filepath));
-    }
 }
